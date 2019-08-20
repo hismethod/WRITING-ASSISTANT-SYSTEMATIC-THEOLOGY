aeead567d1f6979d74478e74b20c2fa85224e7f0
hismethod <his.methods@gmail.com>
2019-08-20 15:00:57 +0900

    modify navbar hide/show trigger

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import classNames from 'classnames';
 import { useScrollPosition } from '../../hooks/useScroll';
 import './Navbar.scss';
 
 const Navbar = ({ position = 'top', children }) => {
   const [visible, setVisible] = useState(true);
-
-  useScrollPosition(({ direction }) => {
-    const showNavbar = direction === 'up';
+  const bodyHeight = document.body.offsetHeight;
+  const bodyTop = 0;
+  
+  useScrollPosition(({ prevPos, currPos, direction }) => {
+    const nearTop = currPos.y > bodyTop;
+    const nearBottom = currPos.y < -4300;
+    const showNavbar = nearTop || nearBottom || (direction === 'up');
     if (showNavbar !== visible) {
       setVisible(showNavbar);
     }
   });
-
   const navClassName = classNames('navbar', `navbar-${position}`, {'navbar--hidden': !visible});
-  
   return (
     <nav className={navClassName} >
       <div className="navbar__content">
